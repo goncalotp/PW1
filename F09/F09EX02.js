2d92f116994e498039aa51164de8bec7e142743c
Gonçalo Perdigão <37213247+goncalotp@users.noreply.github.com>
2019-11-08 11:57:55 +0000

    Add files via upload

--- F09/F09EX02.js
+++ F09/F09EX02.js
@@ -1,54 +1,87 @@
 Vue.component("game-soccer-card", {
- props: ["game"],
- template: `
- <p>{{game.teamName1}} - {{game.teamName2}}</p>`
+    props: ["game"],
+    template: `
+ 
+            <div class="card" style="width: 18rem;">
+                    <img  src="game.stadiumLink" class="card-img-top" >
+                    <div class="card-body" >
+                        <h1 class="card-title"> {{game.stadiumName}} </h1>
+                        <h3 class="card-text"> 
+                        {{getResult(game)}} 
+                        </h3>
+                  <p class="card-text" v-for="goal in game.goals">
+                        {{goal.minute}}'{{goal.name}}
+                 </p>
+   
+            </div>
+            </div>
+ 
+ 
+ `
+,
+ methods: {
+     getResult(game) {
+        //contabilizar os golos da primeira equipa
+        let goals1 = game.goals.filter(
+            goal => (game.teamName1 === goal.playerTeam)
+        ).length
+
+        //contabilizar os golos da segunda equipa
+        let goals2 = game.goals.filter(
+            goal => (game.teamName2 === goal.playerTeam)
+        ).length
+
+        return ` ${game.teamName1}  ${goals1} -  ${goals2} ${game.teamName2}`
+          
+     }
+ }
 })
 
 const vm = new Vue({
-    el:"#app",
+    el: "#app",
     data: {
         games: []
     },
-    created:function() {
+    created: function () {
         this.games = [
             {
-                id:1,
-                date:"12-12-2019",
+                id: 1,
+                date: "12-12-2019",
                 stadiumName: "Estádio da Luz",
-                stadiumLink: "https://cdn.record.pt/images/2013-06/img_920x518$2013_06_14_20_21_00_796962.jpg",
+                stadiumLink: "https://cdn1.newsplex.pt/media/2019/2/12/679305.jpg?type=artigo",
                 teamName1: "Benfica",
-                teamName2: "Porto",
+                teamName2: "Sporting",
                 goals: [
                     {
-                        name:"Seferovic", minute: 3, team:"Benfica"
+                        playerName: "Seferovic", minute: 3, playerTeam: "Benfica"
                     },
                     {
-                        name:"Jota", minute: 23, team:"Benfica"
+                        playerName: "Jota", minute: 23, playerTeam: "Benfica"
                     },
                     {
-                        name:"Vinicius", minute: 72, team:"Benfica"
+                        playerName: "Vinicius", minute: 72, playerTeam: "Benfica"
                     },
                     {
-                      name:"Coates", minute: 92, team:"Sporting"
+                        playerName: "Coates", minute: 92, playerTeam: "Sporting"
                     }
-                ] 
+                ]
             },
             {
-                id:2,
-                date:"13-12-2019",
+                id: 2,
+                date: "13-12-2019",
                 stadiumName: "Estádio do Dragão",
                 stadiumLink: "https://files.app.fcporto.pt/sources/5c90c108c4b3b1J17qAEFdZDq1XiD.jpg",
                 teamName1: "Porto",
                 teamName2: "Aves",
                 goals: [
                     {
-                        name:"Marcano", minute: 10, team:"Porto"
+                        playerName: "Marcano", minute: 10, playerTeam: "Porto"
                     },
-                   
-                ] 
+
+                ]
             }
         ]
     }
 
-   
+
 })
